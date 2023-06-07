5def9a7277ea02c826b26cfa392f4b21bccdc38f
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2023-06-06 23:55:57 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $("#banners ul").bxSlider({
         auto: true
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 63) {
+            $(".container2").addClass("cabecalho-rolagem");
+        } else {
+            $(".container2").removeClass("cabecalho-rolagem");
+        }
+    });
 });  
