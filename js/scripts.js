61edd2c428b859a174dbefbf042f984ea390aaa6
Lucas Daniel <101567841+lucasdanielzx@users.noreply.github.com>
2023-06-08 18:47:52 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,35 @@
 $(document).ready(function(){
-    $("#banners ul").bxSlider({
+    $("#banners ul").bxSlider({ /*slider da tela inicial*/
         auto: true
     });
 
-    $(window).scroll(function () {
+    $(window).scroll(function () { /*fixar menu ao rolar página*/
         if ($(this).scrollTop() > 63) {
-            $(".container2").addClass("cabecalho-rolagem");
+            $(".container-menu-principal").addClass("cabecalho-rolagem");
         } else {
-            $(".container2").removeClass("cabecalho-rolagem");
+            $(".container-menu-principal").removeClass("cabecalho-rolagem");
         }
     });
+
+    $('#conteudo-fotos-videos .box-fotos').magnificPopup({ /*galeria de imagens*/
+		delegate: 'a',
+		type: 'image',
+		closeOnContentClick: false,
+		closeBtnInside: false,
+		mainClass: 'mfp-with-zoom mfp-img-mobile',
+		image: {
+			verticalFit: true,
+		},
+		gallery: {
+			enabled: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300, // don't foget to change the duration also in CSS
+			opener: function(element) {
+				return element.find('img');
+			}
+		}
+		
+	});
 });  
